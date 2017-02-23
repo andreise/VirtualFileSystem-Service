e8046011c8536f45b9ffb590ed5efec9d55bec4c
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-02-24 01:18:43 +0400

    refactor

--- VirtualFileSystem.Service.WindowsService/Program.cs
+++ VirtualFileSystem.Service.WindowsService/Program.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ServiceProcess;
+﻿using System.ServiceProcess;
 
 namespace VirtualFileSystem.Service.WindowsService
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
