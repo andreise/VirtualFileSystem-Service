2f443fc74f6c1a2e03c655aef218dfc882d02c79
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-05-09 20:41:37 +0400

    New Client model connected to client console app

---
+++ VirtualFileSystemClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+namespace VirtualFileSystemClient
+{
+    using Model;
+
+    static class Program
+    {
+        static void Main(string[] args) => new Client(ReadLine, WriteLine).Run().Wait();
+    }
+}
