5e90375a6b5df87562467409346a6d8701eaef0f
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-05-29 08:25:44 +0400

    Serializable to AuthenticateUserException added

--- VirtualFileSystem.ServiceModel/Security/AuthenticateUserException.cs
+++ VirtualFileSystem.ServiceModel/Security/AuthenticateUserException.cs
@@ -2,10 +2,13 @@
 
 namespace VirtualFileSystem.ServiceModel.Security
 {
+
+    [Serializable]
     internal sealed class AuthenticateUserException : Exception
     {
         public AuthenticateUserException(string message) : base(message)
         {
         }
     }
+
 }
