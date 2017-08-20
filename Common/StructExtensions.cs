8e634919462e5d1d399d5512e8766553cdc3ae27
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-08-20 08:31:21 +0400

    Updated Common

--- Common/StructExtensions.cs
+++ Common/StructExtensions.cs
@@ -1,7 +1,15 @@
-﻿namespace Common
+﻿using System;
+
+namespace Common
 {
     public static class StructExtensions
     {
         public static TStruct? AsNullable<TStruct>(this TStruct value) where TStruct : struct => value;
+
+        public static bool IsNullOrDefault<TStruct>(this TStruct? value) where TStruct : struct, IEquatable<TStruct> =>
+            !value.HasValue || value.Value.Equals(default(TStruct));
+
+        public static bool IsNullOrDefault<TStruct>(this TStruct? value, TStruct defaultValue) where TStruct : struct, IEquatable<TStruct> =>
+            !value.HasValue || value.Value.Equals(defaultValue);
     }
 }
